8aa1f1fb54640c54a39df4e6a568df2c95e133a9
jaya Krishna <jayakrishna@jayas-MacBook-Air.local>
2024-04-26 15:25:25 +0530

    updated api endpoint

--- src/env.js
+++ src/env.js
@@ -1,1 +1,2 @@
-export const backendUrl = "http://localhost:9001/api/v1";
+// export const backendUrl = "http://localhost:9001/api/v1";
+export const backendUrl = "https://plaper-admin-backend.onrender.com/api/v1";
